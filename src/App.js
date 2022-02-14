67cba80ac17c0951794e9c24df9ccd91469536b8
etoscano <eleonora.toscano@mail.polimi.it>
2022-02-14 11:10:57 +0100

    Plane rotated

--- src/App.js
+++ src/App.js
@@ -6,19 +6,24 @@ import { Startup } from "./Components/Startup"
 import { Clouds } from "./Components/Clouds"
 import Diamonds from "./diamonds/Diamonds"
 import state from "./Database"
-
+import * as THREE from 'three'
 import Header from "./Components/Header/Header"
 
+
 function App() {
+  const camera = new THREE.PerspectiveCamera(35, 1, 0.1, 100)
+  camera.position.set(0, 0.5, 3)
+
     const scrollArea = useRef()
     const onScroll = e => (state.top.current = e.target.scrollTop)
     useEffect(() => void onScroll({ target: scrollArea.current }), [])
     return (
       <>
-        <Canvas className="canvas" concurrent pixelRatio={1} orthographic camera={{ zoom: state.zoom, position: [0, 0, 500] }}>
+        {/* <Canvas className="canvas" concurrent pixelRatio={1} camera={{ zoom: state.zoom, position: [0, 3, 500]}}> */}
+        <Canvas className="canvas" concurrent pixelRatio={1} camera={camera}>
           <Suspense fallback={<Dom center className="loading" children="Loading..." />}>
 
-          <Clouds  args={[15, 15, 512, 512]} frustumCulled={false} />
+          <Clouds  args={[7,7, 512, 512]} rotation={[- Math.PI * 0.5, 0, 0]} frustumCulled={false} />
  
 
             <Content />
